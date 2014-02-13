ae5ae27bb07891a9eb5ce222336d32c17cc32d7e
fcriscuo <fcriscu1@jhu.edu>
2014-02-12 16:41:44 -0800

    enlarge identifier cache size

--- src/main/java/org/ndexbio/common/cache/NdexIdentifierCache.java
+++ src/main/java/org/ndexbio/common/cache/NdexIdentifierCache.java
@@ -19,99 +19,114 @@ import com.google.common.cache.RemovalNotification;
  */
 public enum NdexIdentifierCache {
 	INSTANCE;
-	private static final Logger logger = LoggerFactory.getLogger(NdexIdentifierCache.class);
-	private static final Long CACHE_SIZE = 1000000L;
-	
-	 private LoadingCache<String, Long> identifierCache; 
-	 /*
-	  * the key for a BaseTern entry is ns:value
-	  * the key for a FunctionTerm  entry is a concatenation of its children's id
-	  * the value is the JdexId for the term itself
-	  */
-	 private LoadingCache<String, Long> termCache;
-	 
-	 
-	 public boolean isNovelTerm(String aTermIdentifier){
-		 if(Strings.isNullOrEmpty(aTermIdentifier)) { return false;}
-		 if( null == this.accessTermCache().getIfPresent(aTermIdentifier)) {
-			 return true;
-		 }
-		 return false;
-	 }
-	 /*
-	  * public method to determine if a non-term identifier has already been 
-	  * registered in the cache
-	  */
-	 public boolean isNovelIdentifier(String identifier){
-		 if(Strings.isNullOrEmpty(identifier)) { return false;}
-		 if(null == this.accessIdentifierCache().getIfPresent(identifier)){
-			 return true;
-		 }
-		 return false;
-	 }
-	 
-	 
-	 public LoadingCache<String,Long> accessTermCache(){
-		 if (null == termCache){
-			 this.initializeTermCache();
-		 }
-		 return termCache;
-	 }
-	 public LoadingCache<String,Long> accessIdentifierCache() {
-		 if (null == identifierCache) {
-			 initializeIdentifierCache();
-		 }
-		 return identifierCache;
-	 }
-	 
-	 
-		//Identifier cache
-		 private RemovalListener<String,Long> identifiertListener = new RemovalListener<String, Long>() {
-			 	
-				public void onRemoval(RemovalNotification<String, Long> removal) {
-					if(!removal.getCause().toString().equalsIgnoreCase("EXPLICIT")){
-					logger.info("*****Identifier removed from cache key= " +removal.getKey()
-							+" Cause= " +removal.getCause().toString());	
-					}
-				}
-				 
-			 };
-	 
-	 private void initializeIdentifierCache() {
-		 this.identifierCache = CacheBuilder.newBuilder()
-				 .maximumSize(CACHE_SIZE)
-				 .expireAfterAccess(120L, TimeUnit.MINUTES)
-				 .removalListener(identifiertListener)
-				 .build(new CacheLoader<String,Long>() {
+	private static final Logger logger = LoggerFactory
+			.getLogger(NdexIdentifierCache.class);
+	private static final Long CACHE_SIZE = 4000000L;
+
+	private LoadingCache<String, Long> identifierCache;
+	/*
+	 * the key for a BaseTern entry is ns:value the key for a FunctionTerm entry
+	 * is a concatenation of its children's id the value is the JdexId for the
+	 * term itself
+	 */
+	private LoadingCache<String, Long> termCache;
+
+	public boolean isNovelTerm(String aTermIdentifier) {
+		if (Strings.isNullOrEmpty(aTermIdentifier)) {
+			return false;
+		}
+		if (null == this.accessTermCache().getIfPresent(aTermIdentifier)) {
+			return true;
+		}
+		return false;
+	}
+
+	/*
+	 * public method to determine if a non-term identifier has already been
+	 * registered in the cache
+	 */
+	public boolean isNovelIdentifier(String identifier) {
+		if (Strings.isNullOrEmpty(identifier)) {
+			return false;
+		}
+		if (null == this.accessIdentifierCache().getIfPresent(identifier)) {
+			return true;
+		}
+		return false;
+	}
+
+	public LoadingCache<String, Long> accessTermCache() {
+		if (null == termCache) {
+			this.initializeTermCache();
+		}
+		return termCache;
+	}
+
+	public LoadingCache<String, Long> accessIdentifierCache() {
+		if (null == identifierCache) {
+			initializeIdentifierCache();
+		}
+		return identifierCache;
+	}
+
+	// Term cache
+	private RemovalListener<String, Long> identifiertListener = new RemovalListener<String, Long>() {
+
+		public void onRemoval(RemovalNotification<String, Long> removal) {
+			if (!removal.getCause().toString().equalsIgnoreCase("EXPLICIT")) {
+				logger.error("*****Identifier removed from cache key= "
+						+ removal.getKey() + " Cause= "
+						+ removal.getCause().toString());
+			}
+		}
+
+	};
+
+	// Identifier cache
+	private RemovalListener<String, Long> termListener = new RemovalListener<String, Long>() {
+
+		public void onRemoval(RemovalNotification<String, Long> removal) {
+			if (!removal.getCause().toString().equalsIgnoreCase("EXPLICIT")) {
+				logger.error("*****Term removed from cache key= "
+						+ removal.getKey() + " Cause= "
+						+ removal.getCause().toString());
+			}
+		}
+
+	};
+
+	private void initializeIdentifierCache() {
+		this.identifierCache = CacheBuilder.newBuilder()
+				.maximumSize(CACHE_SIZE)
+				.expireAfterAccess(180L, TimeUnit.MINUTES)
+				.removalListener(identifiertListener)
+				.build(new CacheLoader<String, Long>() {
 
 					@Override
 					public Long load(String key) throws Exception {
-						
-						//logger.info("Cache created new jdex id for identifier: "
-						//	+key);
+
+						// logger.info("Cache created new jdex id for identifier: "
+						// +key);
 						return JdexIdService.INSTANCE.getNextJdexId();
 					}
-					 
-				 });
-	 }
-	 
-	 
-	 private void initializeTermCache() {
-		 this.termCache = CacheBuilder.newBuilder()
-				 .maximumSize(5000L)
-				 .expireAfterAccess(60L, TimeUnit.MINUTES)
-				 .removalListener(new TermRemovalListener()) 
-				 .build(new CacheLoader<String,Long>() {
-						@Override
-						// the supplied key is not in the cache
-						// generate a new JDEx ID for a new key value pair
-						public Long load(String key) throws Exception {							
-							return JdexIdService.INSTANCE.getNextJdexId();
-						}
-						 
-					 });
-				 
-	 }
-	 
-	 
+
+				});
+	}
+
+	private void initializeTermCache() {
+		this.termCache = CacheBuilder.newBuilder().maximumSize(CACHE_SIZE)
+				.expireAfterAccess(180L, TimeUnit.MINUTES)
+				.removalListener(termListener)
+				.build(new CacheLoader<String, Long>() {
+					@Override
+					// the supplied key is not in the cache
+					// generate a new JDEx ID for a new key value pair
+					public Long load(String key) throws Exception {
+						return JdexIdService.INSTANCE.getNextJdexId();
+					}
+
+				});
+
+	}
+
 }
