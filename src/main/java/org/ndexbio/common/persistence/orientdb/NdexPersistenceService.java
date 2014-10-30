826540f76914bf41c8e89a97d51e301393a39448
Dexter Pratt <dexterpratt.bio@gmail.com>
2014-10-29 19:43:08 -0700

    clean up of BioPAX branch merge

--- src/main/java/org/ndexbio/common/persistence/orientdb/NdexPersistenceService.java
+++ src/main/java/org/ndexbio/common/persistence/orientdb/NdexPersistenceService.java
@@ -1,7 +1,5 @@
 package org.ndexbio.common.persistence.orientdb;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -22,23 +20,21 @@ import org.ndexbio.common.models.object.network.RawCitation;
 import org.ndexbio.common.models.object.network.RawEdge;
 import org.ndexbio.common.models.object.network.RawNamespace;
 import org.ndexbio.common.models.object.network.RawSupport;
-import org.ndexbio.model.object.network.Citation;
-import org.ndexbio.model.object.network.Edge;
-import org.ndexbio.model.object.network.FunctionTerm;
-import org.ndexbio.model.object.network.Network;
-import org.ndexbio.model.object.network.NetworkSummary;
-import org.ndexbio.model.object.network.Support;
-import org.ndexbio.model.object.network.VisibilityType;
+import org.ndexbio.common.util.NdexUUIDFactory;
 import org.ndexbio.model.object.NdexPropertyValuePair;
 import org.ndexbio.model.object.ProvenanceEntity;
 import org.ndexbio.model.object.SimplePropertyValuePair;
-import org.ndexbio.common.util.NdexUUIDFactory;
+import org.ndexbio.model.object.network.Citation;
+import org.ndexbio.model.object.network.Edge;
+import org.ndexbio.model.object.network.FunctionTerm;
 import org.ndexbio.model.object.network.Namespace;
+import org.ndexbio.model.object.network.NetworkSummary;
+import org.ndexbio.model.object.network.Support;
+import org.ndexbio.model.object.network.VisibilityType;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Preconditions;
-import com.orientechnologies.orient.core.metadata.schema.OType;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import com.tinkerpop.blueprints.impls.orient.OrientVertex;
@@ -214,7 +210,7 @@ public class NdexPersistenceService extends PersistenceService {
 		
 		ODocument bTermDoc = elementIdCache.get(baseTermId);
 		if(!bTermDoc.getClassName().equals(NdexClasses.BaseTerm))
-			throw new NdexException ("Element "+ baseTermId +" is not a base term. It is " + 
+			throw new NdexException ("Element " + baseTermId + " is not an instance of  baseTerm. It is " + 
 							bTermDoc.getClassName() );
     	OrientVertex bV = graph.getVertex(bTermDoc);
 		nodeV.addEdge(NdexClasses.Node_E_alias, bV);
@@ -316,7 +312,7 @@ public class NdexPersistenceService extends PersistenceService {
        	return v;
 	}
 
-	// alias is treated as a baseTerm
+	// related term is assumed to be a base term
 	public void addRelatedTermToNode(long nodeId, String[] relatedTerms) throws ExecutionException, NdexException {
 		ODocument nodeDoc = elementIdCache.get(nodeId);
 		OrientVertex nodeV = graph.getVertex(nodeDoc);
@@ -330,7 +326,7 @@ public class NdexPersistenceService extends PersistenceService {
 		elementIdCache.put(nodeId, nodeV.getRecord());
 	}
 
-	// alias is treated as a baseTerm
+	// related term is assumed to be a base term
 	public void addRelatedTermToNode(long nodeId, long baseTermId ) throws ExecutionException {
 		ODocument nodeDoc = elementIdCache.get(nodeId);
 		OrientVertex nodeV = graph.getVertex(nodeDoc);
@@ -340,7 +336,8 @@ public class NdexPersistenceService extends PersistenceService {
 		elementIdCache.put(nodeId, nodeV.getRecord());
 	}
 	
-	// alias is treated as a baseTerm
+	// related term is assumed to be a base term but its not clear that 
+	// we actually constrain the type of the related term
 	private void addRelatedTermToNode(OrientVertex nodeV, long baseTermId ) throws ExecutionException {
 		ODocument bDoc = elementIdCache.get(baseTermId);
 		OrientVertex bV = graph.getVertex(bDoc);
@@ -1046,6 +1043,13 @@ public class NdexPersistenceService extends PersistenceService {
 
 	}
 	
+	public void setNetworkVisibility(VisibilityType visibility) {
+
+		this.networkDoc = this.networkDoc.field(NdexClasses.Network_P_visibility, visibility)
+				.save();
+
+	}
+	
 	public void setNetworkTitleAndDescription(String title, String description) {
 	   if ( description != null ) {
 		   this.network.setDescription(description);
@@ -1096,6 +1100,14 @@ public class NdexPersistenceService extends PersistenceService {
 		OrientVertex v = graph.getVertex(nodeDoc);
 		addPropertiesToVertex ( v, properties, presentationProperties);
 		this.elementIdCache.put(nodeId, v.getRecord());
+	}
+	
+	public void setCitationProperties(Long citationId, Collection<NdexPropertyValuePair> properties, 
+			Collection<SimplePropertyValuePair> presentationProperties) throws ExecutionException, NdexException {
+		ODocument nodeDoc = this.elementIdCache.get(citationId);
+		OrientVertex v = graph.getVertex(nodeDoc);
+		addPropertiesToVertex ( v, properties, presentationProperties);
+		this.elementIdCache.put(citationId, v.getRecord());
 	}
 	
 	/**
